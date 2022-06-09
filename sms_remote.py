2b5c3a45f86f4f47d269731e36b48ddcdb9cfbfd
CYBER SH <85736436+ShTasrif@users.noreply.github.com>
2022-06-09 06:04:28 +0600

    Add files via upload

--- sms_remote.py
+++ sms_remote.py
@@ -1,124 +1,79 @@
 # Encrypted by SH-Encryptor
-# Github- https://github.com/ShTasrif
+# Github- https://github.com/ShTasrif 
 
-sms-remotesms-remotesms-remotesms-remotesms-remote = ""
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x61\x57\x31\x77\x62\x33\x4a\x30\x49\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x39\x7a\x43\x6d\x6c\x74\x63\x47\x39\x79"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x43\x42\x79\x5a\x58\x46\x31\x5a\x58"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4e\x30\x63\x77\x6f\x4b\x49\x30\x4e\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x47\x39\x31\x63\x67\x70\x35\x5a\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x78\x73\x62\x33\x63\x67\x50\x53\x41\x6e"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x58\x44\x4d\x7a\x57\x7a\x6b\x7a\x62\x53"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x4b\x43\x6d\x64\x79\x5a\x57\x56\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x44\x30\x67\x4a\x31\x77\x77\x4d\x7a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4e\x62\x4d\x54\x73\x7a\x4d\x6d\x30\x6e"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x43\x67\x70\x6a\x65\x57\x46\x75\x49\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x41\x39\x49\x43\x4a\x63\x4d\x44\x4d\x7a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x57\x7a\x6b\x32\x62\x53\x49\x4b\x43\x6e"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4a\x6c\x5a\x43\x41\x39\x49\x43\x64\x63"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4d\x44\x4d\x7a\x57\x7a\x6b\x78\x62\x53"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x4b\x43\x6d\x78\x70\x5a\x32\x68\x30"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x59\x6d\x78\x31\x5a\x53\x41\x39\x49\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x63\x4d\x44\x4d\x7a\x57\x7a\x6b\x30"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x53\x63\x4b\x43\x67\x6f\x6a\x52\x45"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x56\x54\x53\x55\x64\x4f\x49\x45\x6c\x4e"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x55\x45\x39\x53\x56\x41\x70\x73\x61\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x35\x6c\x50\x53\x68\x35\x5a\x57\x78\x73"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x33\x63\x72\x49\x6a\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x39\x50\x54\x30\x39"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x50\x54\x30\x39\x50\x54\x30\x39\x50\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x39\x50\x54\x30\x69\x4b\x51\x70\x30"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x6d\x56\x79\x63\x32\x6c\x76\x62\x6a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x30\x6f\x5a\x33\x4a\x6c\x5a\x57\x34\x72"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x6c\x78\x30\x58\x48\x51\x67\x49\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x41\x67\x49\x46\x5a\x6c\x63\x6e\x4e\x70"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x32\x34\x67\x4f\x69\x41\x78\x4c\x6a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x41\x75\x4d\x43\x41\x69\x4b\x51\x6f\x4b"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x47\x56\x32\x62\x32\x78\x76\x63\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x56\x79\x50\x53\x68\x6e\x63\x6d\x56\x6c"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x69\x73\x69\x58\x47\x35\x63\x64\x46"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x78\x30\x52\x47\x56\x32\x62\x32\x78\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x47\x56\x6b\x49\x45\x4a\x35\x49\x44"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x6f\x67\x55\x30\x67\x67\x56\x45\x46\x54"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x55\x6b\x6c\x47\x49\x43\x49\x70\x43\x67"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x70\x30\x62\x6d\x46\x74\x5a\x54\x30\x6f"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x59\x33\x6c\x68\x62\x69\x73\x69\x58\x48"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x52\x63\x64\x43\x41\x67\x34\x70\x79\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x34\x70\x79\x76\x55\x30\x67\x67\x51\x6b"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x51\x67\x55\x30\x31\x54\x49\x45\x4a\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x57\x4a\x6c\x63\x75\x4b\x63\x72\x2b"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4b\x63\x72\x79\x49\x70\x43\x67\x6f\x4b"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x32\x31\x7a\x58\x32\x4e\x6f\x5a\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4e\x72\x50\x58\x4a\x6c\x63\x58\x56\x6c"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x33\x52\x7a\x4c\x6d\x64\x6c\x64\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x67\x69\x61\x48\x52\x30\x63\x48\x4d\x36"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4c\x79\x39\x77\x59\x58\x4e\x30\x5a\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4a\x70\x62\x69\x35\x6a\x62\x32\x30\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x6d\x46\x33\x4c\x7a\x56\x35\x5a\x7a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x6c\x4c\x57\x45\x68\x45\x49\x69\x6b\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x47\x56\x34\x64\x41\x6f\x4b\x61\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x59\x67\x63\x32\x31\x7a\x58\x32\x4e\x6f"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x57\x4e\x72\x50\x54\x30\x69\x54\x30"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x34\x69\x4f\x67\x6f\x4a\x62\x33\x4d\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x33\x6c\x7a\x64\x47\x56\x74\x4b\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4a\x77\x65\x58\x52\x6f\x62\x32\x34\x7a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x48\x4e\x74\x63\x79\x35\x77\x65\x53"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x70\x43\x67\x6b\x4b\x5a\x57\x78\x70"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x69\x42\x7a\x62\x58\x4e\x66\x59\x32"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x68\x6c\x59\x32\x73\x39\x50\x53\x4a\x50"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x52\x6b\x59\x69\x4f\x67\x6f\x4a\x62\x33"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4d\x75\x63\x33\x6c\x7a\x64\x47\x56\x74"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4b\x43\x4a\x6a\x62\x47\x56\x68\x63\x69"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x70\x43\x67\x6c\x76\x63\x79\x35\x7a"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x65\x58\x4e\x30\x5a\x57\x30\x6f\x49\x6d"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x78\x76\x62\x47\x4e\x68\x64\x43\x42\x73"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x32\x64\x76\x4c\x6e\x52\x34\x64\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x70\x43\x67\x6c\x77\x63\x6d\x6c\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x43\x68\x6b\x5a\x58\x5a\x76\x62\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x39\x77\x5a\x58\x49\x70\x43\x67\x6c\x77"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x6d\x6c\x75\x64\x43\x67\x69\x49\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x70\x43\x67\x6c\x77\x63\x6d\x6c\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x43\x68\x30\x62\x6d\x46\x74\x5a\x53"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x6b\x4b\x43\x58\x42\x79\x61\x57\x35\x30"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4b\x43\x63\x6e\x4b\x51\x6f\x4a\x63\x48"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4a\x70\x62\x6e\x51\x6f\x62\x47\x6c\x75"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x53\x6b\x4b\x43\x58\x42\x79\x61\x57"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x35\x30\x4b\x43\x49\x67\x49\x69\x6b\x4b"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x43\x58\x42\x79\x61\x57\x35\x30\x4b\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x78\x70\x5a\x32\x68\x30\x59\x6d\x78\x31"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x53\x73\x69\x58\x48\x52\x63\x64\x46"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x52\x6f\x61\x58\x4d\x67\x56\x47\x39\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x48\x4d\x67\x61\x58\x4d\x67\x64\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x56\x74\x63\x47\x39\x79\x59\x58\x4a\x35"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x69\x41\x72\x59\x33\x6c\x68\x62\x69"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x73\x69\x49\x46\x74\x50\x52\x6b\x5a\x64"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x58\x47\x34\x69\x4b\x51\x6f\x4a\x63\x48"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4a\x70\x62\x6e\x51\x6f\x5a\x33\x4a\x6c"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x5a\x57\x34\x72\x49\x6c\x78\x30\x49\x43"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x41\x67\x55\x47\x78\x6c\x59\x58\x4e\x6c"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x49\x47\x4e\x76\x62\x6e\x52\x68\x59\x33"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x51\x67\x51\x57\x52\x74\x61\x57\x34\x67"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x47\x38\x67\x54\x31\x42\x46\x54\x69"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x42\x55\x61\x47\x55\x67\x56\x47\x39\x76"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x62\x48\x4e\x63\x62\x6c\x78\x75\x49\x69"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x6b\x4b\x43\x57\x39\x7a\x4c\x6e\x4e\x35"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x33\x52\x6c\x62\x53\x67\x69\x65\x47"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x52\x6e\x4c\x57\x39\x77\x5a\x57\x34\x67"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x61\x48\x52\x30\x63\x48\x4d\x36\x4c\x79"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x39\x30\x4c\x6d\x31\x6c\x4c\x33\x52\x68"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x63\x33\x4a\x70\x5a\x6c\x39\x6f\x62\x33"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x4e\x7a\x5a\x57\x35\x66\x63\x32\x68\x31"
-sms-remotesms-remotesms-remotesms-remotesms-remote += "\x64\x6d\x38\x69\x4b\x51\x3d\x3d"
-exec(__import__("\x62\x61\x73\x65\x36\x34").b64decode(sms-remotesms-remotesms-remotesms-remotesms-remote.encode("\x75\x74\x66\x2d\x38")).decode("\x75\x74\x66\x2d\x38"))
+exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
+"😃 😀 🤣  😃 😀 😛  😃 😃 😄  😃 😃 😃  😃 😃 😅  😃 😃 😂  😁 😄  😃 😃 😃  😃 😃 🤣  😃 😀  😃 😀 \
+🤣  😃 😀 😛  😃 😃 😄  😃 😃 😃  😃 😃 😅  😃 😃 😂  😁 😄  😃 😃 😅  😃 😀 😃  😃 😃 😁  😃 😃 😉 \
+ 😃 😀 😃  😃 😃 🤣  😃 😃 😂  😃 😃 🤣  😃 😀  😃 😀  😁 🤣  😂 😉  😃 😃 😃  😃 😀 😊  😃 😃 😃  \
+😃 😃 😉  😃 😃 😅  😃 😀  😃 😄 😃  😃 😀 😃  😃 😀 😊  😃 😀 😊  😃 😃 😃  😃 😃 😛  😁 😄  😂 😃 \
+ 😁 😄  😁 😛  😛 😄  🤣 😃  🤣 😃  😛 😃  🤣 😉  🤣 😃  😃 😀 😛  😁 😛  😃 😀  😃 😀  😃 😀 😁  \
+😃 😃 😅  😃 😀 😃  😃 😀 😃  😃 😃 😀  😁 😄  😂 😃  😁 😄  😁 😛  😛 😄  😅 😊  🤣 😃  🤣 😃  😛 \
+😃  😅 😛  🤣 😛  🤣 😃  🤣 😀  😃 😀 😛  😁 😛  😃 😀  😃 😀  😛 😛  😃 😄 😃  😛 😉  😃 😃 😀  😁\
+ 😄  😁 😄  😂 😃  😁 😄  😁 😅  😛 😄  😅 😊  🤣 😃  🤣 😃  😛 😃  🤣 😉  🤣 😅  😃 😀 😛  😁 😅 \
+ 😃 😀  😃 😀  😃 😃 😅  😃 😀 😃  😃 😀 😀  😁 😄  😂 😃  😁 😄  😁 😛  😛 😄  😅 😊  🤣 😃  🤣 😃\
+  😛 😃  🤣 😉  😅 😛  😃 😀 😛  😁 😛  😃 😀  😃 😀  😃 😀 😊  😃 😀 🤣  😃 😀 😁  😃 😀 😅  😃 😃\
+ 😂  😛 😊  😃 😀 😊  😃 😃 😉  😃 😀 😃  😁 😄  😂 😃  😁 😄  😁 😛  😛 😄  😅 😊  🤣 😃  🤣 😃  \
+😛 😃  🤣 😉  🤣 😄  😃 😀 😛  😁 😛  😃 😀  😃 😀  😃 😀  😁 🤣  😂 😊  😂 😛  😊 😁  😉 😁  😉 😃\
+  😉 😊  😁 😄  😉 😁  😉 😉  😊 😀  😉 😛  😊 😄  😊 😅  😃 😀  😃 😀 😊  😃 😀 🤣  😃 😃 😀  😃 \
+😀 😃  😂 😃  😅 😀  😃 😄 😃  😃 😀 😃  😃 😀 😊  😃 😀 😊  😃 😃 😃  😃 😃 😛  😅 😁  😁 😅  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃\
+  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😂 😃  😁 😅\
+  😅 😃  😃 😀  😃 😃 😂  😃 😃 😊  😃 😀 😃  😃 😃 😅  😃 😃 🤣  😃 😀 🤣  😃 😃 😃  😃 😃 😀  😂 \
+😃  😅 😀  😃 😀 😁  😃 😃 😅  😃 😀 😃  😃 😀 😃  😃 😃 😀  😅 😁  😁 😅  😛 😄  😃 😃 😂  😛 😄  \
+😃 😃 😂  😁 😄  😁 😄  😁 😄  😁 😄  😁 😄  😊 😂  😃 😀 😃  😃 😃 😅  😃 😃 🤣  😃 😀 🤣  😃 😃 😃\
+  😃 😃 😀  😁 😄  🤣 😊  😁 😄  😅 😛  😅 😂  😅 😊  😅 😂  😅 😊  😁 😄  😁 😅  😅 😃  😃 😀  😃\
+ 😀  😃 😀 😀  😃 😀 😃  😃 😃 😊  😃 😃 😃  😃 😀 😊  😃 😃 😃  😃 😃 😄  😃 😀 😃  😃 😃 😅  😂 😃\
+  😅 😀  😃 😀 😁  😃 😃 😅  😃 😀 😃  😃 😀 😃  😃 😃 😀  😅 😁  😁 😅  😛 😄  😃 😃 😀  😛 😄  😃\
+ 😃 😂  😛 😄  😃 😃 😂  😂 😊  😃 😀 😃  😃 😃 😊  😃 😃 😃  😃 😀 😊  😃 😃 😃  😃 😃 😄  😃 😀 😃\
+  😃 😀 😀  😁 😄  😂 😂  😃 😄 😃  😁 😄  🤣 😊  😁 😄  😊 😁  😉 😄  😁 😄  😊 😅  😂 🤣  😊 😁 \
+ 😊 😄  😉 😁  😉 😀  😁 😄  😁 😅  😅 😃  😃 😀  😃 😀  😃 😃 😂  😃 😃 😀  😛 😉  😃 😀 😛  😃 😀\
+ 😃  😂 😃  😅 😀  😛 😛  😃 😄 😃  😛 😉  😃 😃 😀  😅 😁  😁 😅  😛 😄  😃 😃 😂  😛 😄  😃 😃 😂\
+  😁 😄  😁 😄  😃 😀 😀 😁 😃  😃 😀 😀 😁 😃  😊 😁  😉 😄  😁 😄  😂 😂  😂 😊  😁 😄  😊 😁  😉\
+ 😉  😊 😁  😁 😄  😂 😂  😃 😃 😃  😃 😀 😛  😛 😊  😃 😀 😃  😃 😃 😅  😃 😀 😀 😁 😃  😃 😀 😀 😁\
+ 😃  😁 😅  😅 😃  😃 😀  😃 😀  😃 😀  😃 😃 🤣  😃 😀 😛  😃 😃 🤣  😛 🤣  😛 😛  😃 😀 😅  😃 😀\
+ 😃  😛 😛  😃 😀 😉  😂 😃  😃 😃 😅  😃 😀 😃  😃 😃 😁  😃 😃 😉  😃 😀 😃  😃 😃 🤣  😃 😃 😂  \
+😃 😃 🤣  😅 😂  😃 😀 😁  😃 😀 😃  😃 😃 😂  😅 😀  😁 😅  😃 😀 😅  😃 😃 😂  😃 😃 😂  😃 😃 😄 \
+ 😃 😃 🤣  🤣 😊  😅 😉  😅 😉  😃 😃 😄  😛 😉  😃 😃 🤣  😃 😃 😂  😃 😀 😃  😛 😊  😃 😀 🤣  😃 \
+😃 😀  😅 😂  😛 😛  😃 😃 😃  😃 😀 😛  😅 😉  😃 😃 😅  😛 😉  😃 😃 😛  😅 😉  🤣 😁  😃 😄 😃  \
+😃 😀 😁  🤣 😉  😉 🤣  😊 😊  😉 😄  😂 😊  😁 😅  😅 😃  😅 😂  😃 😃 😂  😃 😀 😃  😃 😄 😀  😃 \
+😃 😂  😃 😀  😃 😀  😃 😀 🤣  😃 😀 😄  😁 😄  😃 😃 🤣  😃 😀 😛  😃 😃 🤣  😛 🤣  😛 😛  😃 😀 😅\
+  😃 😀 😃  😛 😛  😃 😀 😉  😂 😃  😂 😃  😁 😅  😉 😛  😉 😊  😁 😅  🤣 😊  😃 😀  😛  😃 😃 😃 \
+ 😃 😃 🤣  😅 😂  😃 😃 🤣  😃 😄 😃  😃 😃 🤣  😃 😃 😂  😃 😀 😃  😃 😀 😛  😅 😀  😁 😅  😃 😃 😄\
+  😃 😄 😃  😃 😃 😂  😃 😀 😅  😃 😃 😃  😃 😃 😀  🤣 😃  😁 😄  😃 😃 🤣  😃 😀 😛  😃 😃 🤣  😅 \
+😂  😃 😃 😄  😃 😄 😃  😁 😅  😅 😃  😃 😀  😛  😃 😀  😃 😀 😃  😃 😀 😊  😃 😀 🤣  😃 😀 😄  😁 \
+😄  😃 😃 🤣  😃 😀 😛  😃 😃 🤣  😛 🤣  😛 😛  😃 😀 😅  😃 😀 😃  😛 😛  😃 😀 😉  😂 😃  😂 😃  \
+😁 😅  😉 😛  😉 😀  😉 😀  😁 😅  🤣 😊  😃 😀  😛  😃 😃 😃  😃 😃 🤣  😅 😂  😃 😃 🤣  😃 😄 😃 \
+ 😃 😃 🤣  😃 😃 😂  😃 😀 😃  😃 😀 😛  😅 😀  😁 😅  😛 😛  😃 😀 😊  😃 😀 😃  😛 😉  😃 😃 😅  \
+😁 😅  😅 😃  😃 😀  😛  😃 😃 😃  😃 😃 🤣  😅 😂  😃 😃 🤣  😃 😄 😃  😃 😃 🤣  😃 😃 😂  😃 😀 😃\
+  😃 😀 😛  😅 😀  😁 😅  😃 😀 😊  😃 😃 😃  😃 😀 😊  😛 😛  😛 😉  😃 😃 😂  😁 😄  😃 😀 😊  😃\
+ 😃 😃  😃 😀 😁  😃 😃 😃  😅 😂  😃 😃 😂  😃 😄 😀  😃 😃 😂  😁 😅  😅 😃  😃 😀  😛  😃 😃 😄 \
+ 😃 😃 😅  😃 😀 🤣  😃 😃 😀  😃 😃 😂  😅 😀  😃 😀 😀  😃 😀 😃  😃 😃 😊  😃 😃 😃  😃 😀 😊  😃\
+ 😃 😃  😃 😃 😄  😃 😀 😃  😃 😃 😅  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  😃 😀 🤣  😃 😃 😀  😃 \
+😃 😂  😅 😀  😁 😅  😁 😄  😁 😅  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  😃 😀 🤣  😃 😃 😀  😃 😃 \
+😂  😅 😀  😃 😃 😂  😃 😃 😀  😛 😉  😃 😀 😛  😃 😀 😃  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  😃 \
+😀 🤣  😃 😃 😀  😃 😃 😂  😅 😀  😁 😛  😁 😛  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  😃 😀 🤣  😃 \
+😃 😀  😃 😃 😂  😅 😀  😃 😀 😊  😃 😀 🤣  😃 😃 😀  😃 😀 😃  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅\
+  😃 😀 🤣  😃 😃 😀  😃 😃 😂  😅 😀  😁 😅  😁 😄  😁 😅  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  \
+😃 😀 🤣  😃 😃 😀  😃 😃 😂  😅 😀  😃 😀 😊  😃 😀 🤣  😃 😀 😁  😃 😀 😅  😃 😃 😂  😛 😊  😃 😀 \
+😊  😃 😃 😉  😃 😀 😃  😅 😁  😁 😅  😛 😄  😃 😃 😂  😛 😄  😃 😃 😂  😊 😅  😃 😀 😅  😃 😀 🤣  \
+😃 😃 🤣  😁 😄  😊 😅  😃 😃 😃  😃 😃 😃  😃 😀 😊  😃 😃 🤣  😁 😄  😃 😀 🤣  😃 😃 🤣  😁 😄  😃\
+ 😃 😂  😃 😀 😃  😃 😀 😛  😃 😃 😄  😃 😃 😃  😃 😃 😅  😛 😉  😃 😃 😅  😃 😄 😃  😁 😅  😁 😄  \
+😅 😁  😛 😛  😃 😄 😃  😛 😉  😃 😃 😀  😅 😁  😁 😅  😁 😄  😛 😃  😉 😛  😉 😀  😉 😀  😛 😁  😛\
+ 😄  😃 😃 😀  😁 😅  😅 😃  😃 😀  😛  😃 😃 😄  😃 😃 😅  😃 😀 🤣  😃 😃 😀  😃 😃 😂  😅 😀  😃\
+ 😀 😁  😃 😃 😅  😃 😀 😃  😃 😀 😃  😃 😃 😀  😅 😁  😁 😅  😛 😄  😃 😃 😂  😁 😄  😁 😄  😁 😄 \
+ 😊 😀  😃 😀 😊  😃 😀 😃  😛 😉  😃 😃 🤣  😃 😀 😃  😁 😄  😛 😛  😃 😃 😃  😃 😃 😀  😃 😃 😂  \
+😛 😉  😛 😛  😃 😃 😂  😁 😄  😂 🤣  😃 😀 😀  😃 😀 😛  😃 😀 🤣  😃 😃 😀  😁 😄  😃 😃 😂  😃 😃\
+ 😃  😁 😄  😉 😛  😊 😀  😂 😛  😉 😊  😁 😄  😊 😅  😃 😀 😅  😃 😀 😃  😁 😄  😊 😅  😃 😃 😃  \
+😃 😃 😃  😃 😀 😊  😃 😃 🤣  😛 😄  😃 😃 😀  😛 😄  😃 😃 😀  😁 😅  😅 😃  😃 😀  😛  😃 😃 😃  \
+😃 😃 🤣  😅 😂  😃 😃 🤣  😃 😄 😃  😃 😃 🤣  😃 😃 😂  😃 😀 😃  😃 😀 😛  😅 😀  😁 😅  😃 😄 😀 \
+ 😃 😀 😀  😃 😀 😁  😅 🤣  😃 😃 😃  😃 😃 😄  😃 😀 😃  😃 😃 😀  😁 😄  😃 😀 😅  😃 😃 😂  😃 😃\
+ 😂  😃 😃 😄  😃 😃 🤣  🤣 😊  😅 😉  😅 😉  😃 😃 😂  😅 😂  😃 😀 😛  😃 😀 😃  😅 😉  😃 😃 😂 \
+ 😛 😉  😃 😃 🤣  😃 😃 😅  😃 😀 🤣  😃 😀 😄  😛 🤣  😃 😀 😅  😃 😃 😃  😃 😃 🤣  😃 😃 🤣  😃 😀\
+ 😃  😃 😃 😀  😛 🤣  😃 😃 🤣  😃 😀 😅  😃 😃 😉  😃 😃 😊  😃 😃 😃  😁 😅  😅 😃"
+.split("  ")])))
